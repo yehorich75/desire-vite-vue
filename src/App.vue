3fe3f6d0635d4f13a36c1ce509428e2f4b8406c7
Yegorich75 <yegorich75@gmail.com>
2021-07-22 17:28:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Header />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from './components/Header.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-carousel-item.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="v-carousel-item">
+        <img :src='require("../assets/images" + item_data.image)'>
+    </div>
+</template>
+
+
+<script lang=" js">
+    export default {
+        name: 'VCarouselItem',
+        props: {
+            item_data: {
+                type: Object,
+                default: () => {}
+            }
+        },
+        data() {
+            return {}
+        },
+        computed: {}
+    }
+</script>
